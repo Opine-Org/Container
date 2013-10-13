abf3685be228d5de24388248a671ac777e80752d
webuser <ryan@virtuecenter.com>
2013-10-13 04:09:12 -0400

    fix bugs

--- src/Container/Container.php
+++ src/Container/Container.php
@@ -6,7 +6,8 @@ class Container {
 	private $parameters = [];
 	private static $instances = [];
 
-	public function __construct ($containerConfig) {
+	public function __construct ($root, $name='/container.yml') {
+		$containerConfig = $root . $name;
 		if (!file_exists($containerConfig)) {
 			throw new \Exception ('Container file not found: ' . $containerConfig);
 		}
@@ -17,6 +18,7 @@ class Container {
 		if ($config == false) {
 			throw new \Exception('Can not parse YAML file: ' . $containerConfig);
 		}
+		$this->parameters['root'] = $root;
 		if (isset($config['parameters']) && is_array($config['parameters'])) {
 			foreach ($config['parameters'] as $parameterName => $parameter) {
 				$this->parameters[$parameterName] = $parameter;
@@ -41,6 +43,9 @@ class Container {
 	}
 
 	public function __get ($serviceName) {
+		if ($serviceName == 'container') {
+			return $this;
+		}
 		if (!isset($this->services[$serviceName])) {
 			throw new \Exception ('Unknown service: ' . $serviceName);
 		}
@@ -50,48 +55,57 @@ class Container {
 			$scope = $service['scope'];
 		}
 		$arguments = [];
-		if (isset($service['arguments'])) {
-			$arguments = $this->_arguments($serviceName, $service['arguments'], 'construct');
-		}
 		if ($scope == 'container') {
 			if (!isset(self::$instances[$serviceName])) {
+				if (isset($service['arguments'])) {
+					$arguments = $this->_arguments($serviceName, $service['arguments'], 'construct');
+				}
 				$rc = new \ReflectionClass($service['class']);
 				self::$instances[$serviceName] = $rc->newInstanceArgs($arguments);
+				$this->_calls($serviceName, $service, self::$instances[$serviceName]);		
 			}
-			$serviceInstance = self::$instances[$serviceName];
+			return self::$instances[$serviceName];
 		} elseif ($scope == 'prototype') {
+			if (isset($service['arguments'])) {
+				$arguments = $this->_arguments($serviceName, $service['arguments'], 'construct');
+			}
 			$rc = new \ReflectionClass($service['class']);
 			$serviceInstance = $rc->newInstanceArgs($arguments);
 		} else {
 			throw new \Exception('Unknown container scope: ' . $scope);
 		}
-		if (isset($service['calls']) && is_array($service['calls'])) {
-			foreach ($service['calls'] as $call) {
-				if (!is_array($call) || empty($call)) {
-					throw new \Exception('Invalid Service Call for: ' . $serviceName);
-				}
-				$arguments = [];
-				if (isset($call[1]) && is_array($call[1])) {
-					$arguments = $this->_arguments($serviceName, $call[1], 'call');
-				}
-				call_user_func_array([$serviceInstance, $call[0]], $arguments);
-			}
-		}
+		$this->_calls($serviceName, $service, $serviceInstance);
 		return $serviceInstance;
 	}
 
-	private function _arguments ($servicenName, &$arguments, $type) {
+	private function _calls ($serviceName, $service, $serviceInstance) {
+		if (!isset($service['calls']) || !is_array($service['calls'])) {
+			return;
+		}
+		foreach ($service['calls'] as $call) {
+			if (!is_array($call) || empty($call)) {
+				throw new \Exception('Invalid Service Call for: ' . $serviceName);
+			}
+			$arguments = [];
+			if (isset($call[1]) && is_array($call[1])) {
+				$arguments = $this->_arguments($serviceName, $call[1], 'call');
+			}
+			call_user_func_array([$serviceInstance, $call[0]], $arguments);
+		}
+	}
+
+	private function _arguments ($serviceName, &$arguments, $type) {
 		if (!is_array($arguments)) {
 			return [];
 		}
 		$argumentsOut = [];
 		foreach ($arguments as $argument) {
-			$argumentsOut[] = $this->_argument($servicenName, $argument, $type);
+			$argumentsOut[] = $this->_argument($serviceName, $argument, $type);
 		}
 		return $argumentsOut;
 	}
 
-	private function _argument ($servicenName, $argument, $type) {
+	private function _argument ($serviceName, $argument, $type) {
 		$first = substr($argument, 0, 1);
 		$second = substr($argument, 1, 1);
 		switch ($first) {
@@ -102,13 +116,17 @@ class Container {
 						$arguments[] = null;
 						break;
 					}
-					throw new \Excpetion($servicenName . ' ' . $type . ' requires parameter ' . $parameter . ', not set');
+					throw new \Excpetion($serviceName . ' ' . $type . ' requires parameter ' . $parameter . ', not set');
 				}
-				return $this->_argument($servicenName, $this->parameters[$parameter], $type);
+				return $this->_argument($serviceName, $this->parameters[$parameter], $type);
 				break;
 
 			case '@':
-				return $this->__get(substr($argument, 1));
+				$argService = substr($argument, 1);
+				if ($serviceName == $argService) {
+					throw new \Exception('Circular reference, ' . $serviceName . ' references ' . $serviceName);
+				}
+				return $this->__get($argService);
 				break;
 
 			default:
