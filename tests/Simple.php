3a7590bc2748fdff8143481ee7cf670b579f94ef
webuser <ryan@virtuecenter.com>
2014-02-02 18:11:58 -0500

    clean-up, copyright, etc

--- tests/Simple.php
+++ tests/Simple.php
@@ -5,46 +5,46 @@ require __DIR__ . '/../src/Container.php';
 use Container\Container;
 
 class One {
-	public $first;
-	public $second;
-	public $third;
+    public $first;
+    public $second;
+    public $third;
 
-	public function __construct ($first, $second) {
-		$this->first = $first;
-		$this->second = $second;
-	}
+    public function __construct ($first, $second) {
+        $this->first = $first;
+        $this->second = $second;
+    }
 
-	public function third ($third) {
-		$this->third = $third;
-	}
+    public function third ($third) {
+        $this->third = $third;
+    }
 
-	public function show () {
-		echo $this->first, "\n", $this->second, "\n", $this->third, "\n";
-	}
+    public function show () {
+        echo $this->first, "\n", $this->second, "\n", $this->third, "\n";
+    }
 }
 
 class Two {
-	private $one;
+    private $one;
 
-	public function __construct (\Test\One $one) {
-		$this->one = $one;
-	}
+    public function __construct (\Test\One $one) {
+        $this->one = $one;
+    }
 
-	public function show () {
-		echo strtoupper($this->one->first), "\n", strtoupper($this->one->second), "\n", strtoupper($this->one->third), "\n";
-	}
+    public function show () {
+        echo strtoupper($this->one->first), "\n", strtoupper($this->one->second), "\n", strtoupper($this->one->third), "\n";
+    }
 }
 
 class Three {
-	private $one;
+    private $one;
 
-	public function __construct (\Test\One $one) {
-		$this->one = $one;
-	}
+    public function __construct (\Test\One $one) {
+        $this->one = $one;
+    }
 
-	public function show () {
-		echo 'X', $this->one->first, "\n", 'X', $this->one->second, "\n", 'X', $this->one->third, "\n";
-	}
+    public function show () {
+        echo 'X', $this->one->first, "\n", 'X', $this->one->second, "\n", 'X', $this->one->third, "\n";
+    }
 }
 
 $container = new Container(__DIR__ . '/test.yml');
